4a5efec2551c95efa19dcb0a26439267e0fbe953
andy941 <finocchio.andrea94@gmail.com>
2021-05-17 22:11:32 +0100

    Ch15 ex6

--- PART_Two/Chapter15/ex6_BarChart.cpp
+++ PART_Two/Chapter15/ex6_BarChart.cpp
@@ -11,13 +11,101 @@
 
 #include "../hello_fltk/Window.h"  
 #include "../hello_fltk/Graph.h"
+#include<numeric>
 
 using namespace Graph_lib;
 
+// I used a modified version of the famous iris dataset. I will calculate the means of the 
+// first three columns and draw a barchart.
+
+//bool find (const string& name, const vector<string>& vec) 
+//{
+//	for (string x : vec ) {
+//		if ( x == name ) return true;
+//	}
+//	return false;
+//}
+
+vector<string> split ( const string& line, const string& delim = ",")
+{
+	char x;
+	vector<string> v;
+	string token;
+	istringstream iss { line };
+
+	while ( iss >> x ) {
+
+		if ( x == ',' ) {
+			v.push_back(token);
+			token = {};
+			continue;
+		}
+
+		token.push_back(x);
+	}
+
+	v.push_back(token);
+
+	return v;
+}
+
+vector<double> split_val ( const string& line, const string& delim = ",")
+{
+	char x;
+	double val;
+	vector<double> v;
+	istringstream iss { line };
+
+	while (iss >> val) {
+		v.push_back(val);
+		iss >> x;
+		if (x != ',') error("This doesn't seem a csv file... \n");
+	}
+
+	return v;
+}
+
+double mean( vector<double> L )
+{
+	   return accumulate( L.begin(), L.end(), 0.0 ) / (double)L.size();
+}
+
 struct DataFrame
 {
+	vector<string> colnames;
+	vector<vector<double>> values;
+	vector<double> means;
+
+	DataFrame(vector<string> cn) :colnames{cn} { values = vector<vector<double>>(cn.size()); };
+	void add_entry(vector<double> v);
+	void calculate_means();
 };
 
+void DataFrame::add_entry(vector<double> v) 
+{ 
+	if (v.size() != colnames.size()) error("wrong number of values"); 
+
+	for ( int i = 0; i < v.size(); i++ ) {
+		values[i].push_back(v[i]); 
+	}
+}
+
+void DataFrame::calculate_means()
+{
+	for ( vector<double> x : values ) { means.push_back( mean(x)); }
+}
+
+//{
+//		if ( find( name, colnames ) ) {
+//			for (int i =0 ; i < colnames.size(); i++) {
+//				if (name == colnames[i]) values[i].push_back(value); return;
+//			}
+//		} else { 
+//			colnames.push_back(name); 
+//			values.push_back(vector<double> {value}); 
+//		}
+//	}
+//
 
 class BarChart
 {
@@ -56,7 +144,28 @@ int main()
 	win.attach(x);
 	win.attach(y);
 
-	gui_main();
+//-----------------------------------------------------------------------------
+
+	ifstream ifs { "iris.csv" };
+
+	string line;
+	
+	getline(ifs, line);
+	vector<string> colnames = split(line, ",");
+
+	DataFrame df { colnames };
+
+	while ( getline(ifs, line) ) {
+		vector<double> values = split_val(line, ",");
+		df.add_entry(values);
+	}
+
+	df.calculate_means();
+	for (double x : df.means) cerr << x << '\n';
+
+
+//-----------------------------------------------------------------------------
+	//gui_main();
 }
 
 
