9f2ee69abd77596660a7d8cc1adb8ecb760715c6
andy941 <finocchio.andrea94@gmail.com>
2021-10-16 00:03:43 +0100

    Drill.cpp

--- PART_Three/Chapter26/Drill.cpp
+++ PART_Three/Chapter26/Drill.cpp
@@ -1,19 +1,15 @@
-
 #include "../std_lib_facilities.h"
 using namespace std;
 
-template<class RAIter, class T>
-bool alg(RAIter b, RAIter e, T t)
+	template<class RAIter, class T> bool alg(RAIter b, RAIter e, T t)
 {
-	cerr << "Random iterator Bsearch called." << endl;
-	if (b==e) return false;
+	if (b==e) return true;	// Left this error on purpose to trigger test
 
 	if (t > *(e-1)) return false;
 	if (t < *b) return false;
 
 	RAIter m = b + (e-b)/2;
 	while (b <= e) {
-		cout << *b << ' ' << *m << ' ' << *e << endl;
 		if (t == *m) return true;
 		if (t <= *m) e = m-1;
 		if (t >= *m) b = m+1;
@@ -22,15 +18,62 @@ bool alg(RAIter b, RAIter e, T t)
 	return false;
 }
 
+struct Test {
+	string label {""};
+	int val {0};
+	vector<int> seq {};
+	bool res {false};
+	Test() = default;
+};
+
+istream& operator>>(istream& is, Test& t)
+{   
+	char r1;
+	char r2;
+	char l1;
+	char l2;
+	is >> l1 >> t.label >> t.val >> l2;
+	if (l1 != '{' || l2 != '{') {
+		is.clear(std::ios::failbit);
+		return is;
+	}
+
+	for ( int i; is >> i;) {
+		t.seq.push_back(i);
+	}
+	is.clear();
+
+	is >> r1 >> t.res >> r2;
+	if (r1 != '}' || r2 != '}') {
+		is.clear(std::ios::failbit);
+		return is;
+	}
+
+	return is;
+}
+
+int test_all(istream& is)
+{
+	int error_count = 0;
+	for (Test t; is>>t; t = {}) {
+		bool r = alg(t.seq.begin(), t.seq.end(), t.val);
+		if (r !=t.res) {
+			cout << "failure: test " << t.label
+				<< " binary_search: "
+				<< t.seq.size() << " elements, val==" << t.val
+				<< " –> " << t.res << '\n';
+		  ++error_count;
+		}
+
+	}
+
+	return error_count;
+}
+
 int main () 
 {
-	constexpr long long int sz { 1000000 };
-	vector<int> v(sz);
-	constexpr long long int n { sz/2 + 100 };
-
-	iota(v.begin(), v.end(), 0);
-	sort(v.begin(), v.end());
-	if (alg(v.begin(), v.end(), n)) cout << "Found number: " << n << endl;
-	else  cout << "NOT Found number: " << n << endl;
-	 
+    ifstream ifs {"tests.txt"};
+	if (!ifs) return 1;
+	int errors = test_all(ifs);
+	cout << "number of errors: " << errors << "\n";
 }
