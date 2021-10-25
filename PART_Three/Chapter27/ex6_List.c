01f11c849dcf821ccd4ba9c8ac53455c65390c18
Andrea Finocchio <finocchio.andrea94@gmail.com>
2021-10-25 18:06:37 +0100

    Chapter27 ex6

--- PART_Three/Chapter27/ex6_List.c
+++ PART_Three/Chapter27/ex6_List.c
@@ -6,6 +6,11 @@
 
 // in this implementation there is an array of links on the stack (pool)
 // the link is "free" if pre and suc are == -1.
+// The exercise says to not change the interface to the list but I don't
+// see how that is possible. if you ask for a link form the pool it will not
+// contain any information about where it is on the array. I changed the
+// interface a bit to simplify the operations and meke them hopefully more
+// efficient.
 
 struct Link { /* link for doubly-linked list */
   int pre;
@@ -25,19 +30,18 @@ struct List *create();          /* make a new empty list on free store */
 void clear(struct List *lst);   /* free all elements of lst */
 void destroy(struct List *lst); /* free all elements of lst, then free lst */
 
-void push_back(struct List *lst, struct Link *p); /* add p at end of lst */
-void push_front(struct List *, struct Link *p);   /* add p at front of lst */
+int push_back(struct List *lst, char *s); /* add p at end of lst */
 /* insert q before p in lst: */
-void insert(struct List *lst, struct Link *p, struct Link *q);
-struct Link *erase(struct List *lst, struct Link *p); /* remove p from lst */
+int insert(struct List *lst, char *s, int n);
+struct Link *erase(struct List *lst, int n); /* remove p from lst */
 
 // Helper function to get Links from the pool
-struct Link *get_link(struct List *lst) {
+int get_link(struct List *lst) {
   for (int i = 0; i < MAX_LIST_SIZE; i++) {
-    if (lst->pool[i].pre == -1 && lst->pool[i].suc == -1)
-      return &(lst->pool[i]);
+    if (lst->pool[i].pre == -1 && lst->pool[i].suc == -1 && lst->pool[i].p == 0)
+      return i;
   }
-  return 0;
+  return -1; // pool is full
 };
 
 void init(struct List *lst) /* initialize *lst to the empty list */
@@ -65,12 +69,7 @@ void clear(struct List *lst) /* free all elements of lst */
   {
     int curr = lst->first;
     free(lst->pool);
-    // while (curr) {
-    //   int next = lst->pool[curr].suc;
-    //   free(&(lst->pool[curr].p));
-    //   curr = next;
-    // }
-    lst->first = lst->last = 0;
+    lst->first = lst->last = -1;
   }
 }
 
@@ -81,66 +80,122 @@ void destroy(struct List *lst) /* free all elements of lst; then free lst */
   free(lst);
 }
 
-struct Name {
-  struct Link lnk; /* the Link required by List operations */
-  char *p;         /* the name string */
-};
-
-struct Name *make_name(char *n) {
-  struct Name *p = (struct Name *)malloc(sizeof(struct Name));
-  p->p = n;
-  return p;
-}
-
-void push_back(struct List *lst, struct Link *p) /* add p at end of lst */
+int push_back(struct List *lst, char *c) /* add p at end of lst */
 {
   assert(lst);
+  int l = get_link(lst);
+  if (l == -1)
+    return -1;
+  lst->pool[l].p = c;
+
   {
     int last = lst->last;
-    if (last) {
-      last->suc = p;
-      p->pre = last;
+    if (last != -1) {
+      lst->pool[last].suc = l;
+      lst->pool[l].pre = last;
     } else {
-      lst->first = p;
-      p->pre = 0;
+      lst->first = l;
+      lst->pool[l].pre = -1;
     }
-    lst->last = p;
-    p->suc = 0;
+    lst->last = l;
+    lst->pool[l].suc = -1;
   }
+  return 1;
 }
 
-struct Link *erase(struct List *lst, struct Link *p)
+struct Link *erase(struct List *lst, int nn)
 /*
           remove p from lst;
          return a pointer to the link after p
 */
 {
   assert(lst);
-  if (p == 0)
-    return 0; /* OK to erase(0) */
+  if (nn < 0)
+    return 0;
+  if (lst->first == lst->last)
+    return 0;
 
-  if (p == lst->first) {
-    if (p->suc) {
+  struct Link *p = &lst->pool[lst->first];
+
+  for (int i = 0; i < nn; i++) {
+    p = &lst->pool[p->suc];
+  }
+
+  p->p = 0;
+
+  if (p->pre == -1) {
+    if (p->suc != -1) {
       lst->first = p->suc;
-      p->suc->pre = 0;
-      return p->suc;
+      lst->pool[p->suc].pre = -1;
+      return &(lst->pool[p->suc]);
     } else {
-      lst->first = lst->last = 0;
+      lst->first = lst->last = -1;
       return 0;
     }
-  } else if (p == lst->last) {
-    if (p->pre) {
+  } else if (p->suc == -1) {
+    if (p->pre != -1) {
       lst->last = p->pre;
-      p->pre->suc = 0;
+      lst->pool[p->suc].pre = -1;
     } else {
-      lst->first = lst->last = 0;
+      lst->first = lst->last = -1;
       return 0;
     }
   }
   // else ...
-  p->suc->pre = p->pre;
-  p->pre->suc = p->suc;
-  return p->suc;
+  lst->pool[p->suc].pre = p->pre;
+  lst->pool[p->pre].suc = p->suc;
+  return &(lst->pool[p->suc]);
+}
+
+int insert(struct List *lst, char *c, int nn) {
+  if (c == 0 || nn < 0)
+    return -1;
+
+  assert(lst);
+  int l = get_link(lst);
+  if (l == -1)
+    return -1;
+
+  struct Link *p = &lst->pool[lst->first];
+
+  for (int i = 0; i < nn; i++) {
+    p = &lst->pool[p->suc];
+  }
+
+  printf("%s\n", p->p);
+  lst->pool[l].p = c;
+
+  {
+    int first = lst->first;
+    if (p->pre == -1) {
+      lst->pool[first].pre = l;
+      lst->pool[l].suc = first;
+      lst->first = l;
+      lst->pool[l].pre = -1;
+    } else {
+      lst->pool[l].pre = p->pre;
+      lst->pool[l].suc = lst->pool[p->pre].suc;
+      lst->pool[p->pre].suc = l;
+      p->pre = l;
+    };
+    return l;
+  }
+};
+
+// Helper print function ------------------------------------------------------
+void print(struct List *names) {
+  int count = 0;
+  struct Link *curr;
+  curr = &(names->pool[names->first]); /* write out all names */
+  while (1) {
+    printf("element %d pre = %d suc = %d: %s\n", count, curr->pre, curr->suc,
+           curr->p);
+    if (curr->suc == -1)
+      break;
+    curr = &(names->pool[curr->suc]);
+    count++;
+  }
+  printf("first: %d\tlast: %d\n", names->first, names->last);
 }
 
 int main() {
@@ -148,29 +203,24 @@ int main() {
   struct List names; /* make a list */
   struct Link *curr;
   init(&names);
+  print(&names);
+  printf("---------------------------------------\n");
 
-  ///* make a few Names and add them to the list: */
-  // push_back(&names, (struct Link *)make_name("Norah"));
-  // push_back(&names, (struct Link *)make_name("Annemarie"));
-  // push_back(&names, (struct Link *)make_name("Kris"));
-  // push_front(&names, (struct Link *)make_name("Three"));
-  // push_front(&names, (struct Link *)make_name("Two"));
-  // push_front(&names, (struct Link *)make_name("One"));
-  // printf("first: %s\tlast: %s\n", ((struct Name *)names.first)->p,
-  //        ((struct Name *)names.last)->p);
-  // insert(&names, names.first, (struct Link *)make_name("NEW_FIRST"));
-  // printf("first: %s\tlast: %s\n", ((struct Name *)names.first)->p,
-  //        ((struct Name *)names.last)->p);
-  // insert(&names, names.last, (struct Link *)make_name("SECOND_TO_LAST"));
-  // printf("first: %s\tlast: %s\n", ((struct Name *)names.first)->p,
-  //        ((struct Name *)names.last)->p);
-
-  /////* remove the second name (with index 1): */
-  // erase(&names, advance(names.first, 2));
-
-  // curr = names.first; /* write out all names */
-  // for (; curr != 0; curr = curr->suc) {
-  //   count++;
-  //   printf("element %d: %s\n", count, ((struct Name *)curr)->p);
-  // }
+  /* make a few Names and add them to the list: */
+  push_back(&names, "Norah");
+  print(&names);
+  printf("---------------------------------------\n");
+  push_back(&names, "Annemarie");
+  print(&names);
+  printf("---------------------------------------\n");
+  push_back(&names, "Kris");
+  print(&names);
+  printf("Insert at 1 ---------------------------\n");
+  insert(&names, "Inserted 1", 1);
+  print(&names);
+  //* remove the second name (with index 1): */
+  printf("Erase 1 -------------------------------\n");
+  erase(&names, 1);
+  print(&names);
+  printf("---------------------------------------\n");
 }
